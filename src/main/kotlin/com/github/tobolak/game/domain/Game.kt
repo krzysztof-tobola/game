2b48db8b62c63cb4e44efd2ab4daca34b9abbdcd
Krzysztof Tobola <ktobola@guidewire.com>
2018-04-06 13:47:05 +0200

    Cleanup in Game

--- src/main/kotlin/com/github/tobolak/game/domain/Game.kt
+++ src/main/kotlin/com/github/tobolak/game/domain/Game.kt
@@ -20,40 +20,48 @@ class Game(
     private val player = Character("player")
     private val item = Character("item")
     private val box = Character("box")
-    private val nextMove = AtomicReference({ b: Board -> b })
+    private val nextMove = AtomicReference { b: Board -> b }
     private val board = AtomicReference(Board(
             bounds,
             CharacterState(player, playerSprite, playerPos),
             CharacterState(item, getSprite("item"), itemPos),
             CharacterState(box, getSprite("box"), boxPos),
-            CharacterState(Character("npc"),getSprite("npc"),npcPos)
+            CharacterState(Character("npc"), getSprite("npc"), npcPos)
     ))
 
     fun tick() {
         val updated = board.updateAndGet { oldBoard ->
-            val board = nextMove.get().invoke(oldBoard)
-
-            if (board.detectCollision(player, item)) {
-                board.replaceSprite(player, playerWithItemSprite).remove(item)
-            } else if (board.detectCollision(player, box)) {
-                if (oldBoard.sprite(player).contains(playerWithItemSprite)) {
-                    oldBoard.replaceSprite(box, getSprite("box-item"))
-                            .replaceSprite(player, playerSprite)
-                } else
-                    oldBoard
-            } else {
-                board
-            }
+            val newBoard = nextMove.get()(oldBoard)
+            handlePlayerMove(newBoard, oldBoard)
         }
 
         renderer.render(updated::paint)
     }
 
-    fun moveLeft() = nextMove.set { it.moveLeft(player) }
-    fun moveRight() = nextMove.set { it.moveRight(player) }
-    fun moveDown() = nextMove.set { it.moveDown(player) }
-    fun moveUp() = nextMove.set { it.moveUp(player) }
+    private fun handlePlayerMove(newBoard: Board, oldBoard: Board): Board {
+        return if (newBoard.detectCollision(player, item)) {
+            newBoard.replaceSprite(player, playerWithItemSprite).remove(item)
+        } else if (newBoard.detectCollision(player, box)) {
+            if (oldBoard.sprite(player).contains(playerWithItemSprite)) {
+                oldBoard.replaceSprite(box, getSprite("box-item"))
+                        .replaceSprite(player, playerSprite)
+            } else {
+                oldBoard
+            }
+        } else {
+            newBoard
+        }
+    }
+
+    fun moveLeft() = movePlayer { it::moveLeft }
+    fun moveRight() = movePlayer { it::moveRight }
+    fun moveDown() = movePlayer { it::moveDown }
+    fun moveUp() = movePlayer { it::moveUp }
     fun stopMoving() = nextMove.set { it }
+
+    private fun movePlayer(getMove: (Board) -> ((Character) -> Board)) = nextMove.set { board ->
+        getMove(board)(player)
+    }
+
     private fun getSprite(name: String) = spritesRepository.byName(name)
 }
-
